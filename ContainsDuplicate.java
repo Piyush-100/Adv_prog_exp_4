4daec776ed79fcc10320644b6b35a7f40998326c
Piyush-100 <157004163+Piyush-100@users.noreply.github.com>
2025-02-10 20:22:23 +0530

    Add files via upload

---
+++ ContainsDuplicate.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        System.out.println("Contains duplicate? " + containsDuplicate(nums));
+    }
+}
